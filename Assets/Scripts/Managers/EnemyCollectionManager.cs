7c4b5644644d96fab976951b907c4d6b91bf4477
Quazards <159225295+Quazards@users.noreply.github.com>
2024-11-17 01:47:37 +0700

    Progress Week 9

---
+++ Assets/Scripts/Managers/EnemyCollectionManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyCollectionManager : MonoBehaviour
+{
+    public static EnemyCollectionManager Instance;
+
+    [SerializeField] private AllEnemyStatsCollection allEnemyColletion;
+
+    public EnemyHealth enemyHealth;
+    public Deck enemyDeck;
+
+    private List<EnemyStats> allEnemies = new List<EnemyStats>();
+    public List<EnemyStats> regularEnemies = new List<EnemyStats>();
+    public List<EnemyStats> miniBossEnemies = new List<EnemyStats>();
+    public List<EnemyStats> bossEnemies = new List<EnemyStats>();
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        enemyDeck = GameObject.FindGameObjectWithTag("EnemyDeck").GetComponent<Deck>();
+        enemyHealth = GameObject.FindGameObjectWithTag("EnemyHealth").GetComponent<EnemyHealth>();
+    }
+
+    public void AddEnemyStats()
+    {
+        allEnemies = allEnemyColletion.AllEnemy.ToList();
+
+        foreach (EnemyStats enemy in allEnemies)
+        {
+            switch (enemy.enemyType)
+            {
+                case EnemyType.Regular:
+                    regularEnemies.Add(enemy);
+                    break;
+                case EnemyType.Miniboss:
+                    miniBossEnemies.Add(enemy);
+                    break;
+                case EnemyType.Boss:
+                    bossEnemies.Add(enemy);
+                    break;
+            }
+        }
+
+    }
+
+    private EnemyStats GetRandomEnemyStat(EnemyType enemyType)
+    {
+        List<EnemyStats> selectedList = null;
+
+        switch(enemyType)
+        {
+            case EnemyType.Regular:
+                selectedList = regularEnemies;
+                break;
+            case EnemyType.Miniboss:
+                selectedList = miniBossEnemies;
+                break;
+            case EnemyType.Boss:
+                selectedList = bossEnemies;
+                break;
+        }
+
+        if (selectedList != null && selectedList.Count > 0)
+        {
+            int randomIndex = Random.Range(0, selectedList.Count);
+
+            return selectedList[randomIndex];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void AssignEnemyStat(EnemyType enemyType)
+    {
+        EnemyStats selectedEnemyStat =  GetRandomEnemyStat(enemyType);
+
+        if(selectedEnemyStat != null)
+        {
+            int newMaxHealth = selectedEnemyStat.maxHealth;
+            enemyHealth.SetEnemyMaxHealth(newMaxHealth);
+
+            enemyDeck.ClearAll();
+            enemyDeck.currentDeck = selectedEnemyStat.deckList;
+            enemyDeck.drawAmount = selectedEnemyStat.enemyDrawAmount;
+
+
+            Debug.Log($"Assigned {selectedEnemyStat.enemyType} with max health: {selectedEnemyStat.maxHealth} and decklist: {selectedEnemyStat.deckList}");
+        }
+        else
+            Debug.LogWarning("No enemy stats available for the selected type.");
+    }
+}
---
+++ Assets/Scripts/Managers/PlayerDeckManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeckManager : MonoBehaviour
+{
+    public static PlayerDeckManager Instance;
+
+    public Deck playerDeck;
+
+    public int startEncounterDraw = 2;
+
+    [SerializeField] private CardPile starterDeck;
+
+    [SerializeField] private CardPile tempDeck;
+
+    public List<ScriptableCard> globalDeck = new();
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void BuildStarterDeck()
+    {
+        globalDeck.AddRange(starterDeck.cardsInPile);
+
+        tempDeck.cardsInPile.AddRange(globalDeck);
+    }
+
+    private void BuildDeck()
+    {
+        tempDeck.cardsInPile.AddRange(globalDeck);
+    }
+
+    public void PlayerStartEncounter()
+    {
+        if (playerDeck.currentDeck == null)
+        {
+            tempDeck.cardsInPile.Clear();
+            playerDeck.currentDeck = tempDeck;
+            BuildStarterDeck();
+        }
+        else
+        {
+            tempDeck.cardsInPile.Clear();
+            BuildDeck();
+        }
+
+        playerDeck.InstantiateDeck();
+        playerDeck.drawAmount += startEncounterDraw;
+        playerDeck.TurnStartDraw();
+        playerDeck.drawAmount -= startEncounterDraw;
+    }
+
+    public void PlayerEndEncounter()
+    {
+        playerDeck.ClearAll();
+        tempDeck.cardsInPile.Clear();
+    }
+
+    public void AddCardToDeck(ScriptableCard card)
+    {
+        globalDeck.Add(card);
+        Debug.Log($"Added {card.card_Name} to player deck.");
+    }
+
+    public void RemoveCardFromDeck(ScriptableCard card)
+    {
+        globalDeck.Remove(card);
+        Debug.Log($"Removed {card.card_Name} from player deck.");
+    }
+}
